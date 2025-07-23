6968de942185d6b34d887bbca83938d0caa28e94
oderahub <okere_chidera@yahoo.com>
2025-07-23 01:09:09 +0100

    feat: hardhart section fix

---
+++ hardhat/README.md
@@ -0,0 +1,5 @@
+hardhat sepolia Deployed Addresses
+
+StorageModule#Storage - 0x287974951879D77AdEcd8B115D2d16ef396B464c
+
+hardhat CoreDAO deployed Address=
---
+++ hardhat/hardhat.config.ts
@@ -0,0 +1,42 @@
+// hardhat.config.js
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
+import '@nomicfoundation/hardhat-verify'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const { PRIVATE_KEY, ETHERSCAN_API_KEY, SEPOLIA_RPC_URL } = process.env
+
+const config: HardhatUserConfig = {
+  solidity: '0.8.28',
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: PRIVATE_KEY ? [`0x${PRIVATE_KEY}`] : []
+    },
+    testnet: {
+      url: 'https://rpc.test2.btcs.network',
+      accounts: PRIVATE_KEY ? [`0x${PRIVATE_KEY}`] : [],
+      chainId: 1114,
+      gasPrice: 1000000000
+    }
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY || ''
+    },
+    customChains: [
+      {
+        network: 'testnet',
+        chainId: 1114,
+        urls: {
+          apiURL: 'https://api.test2.btcs.network/api',
+          browserURL: 'https://scan.test2.btcs.network/'
+        }
+      }
+    ]
+  }
+}
+
+export default config
